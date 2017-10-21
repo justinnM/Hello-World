a25adec0457cd1e3446f37a1ab7e567757ed5c4d
justinnM <32797324+justinnM@users.noreply.github.com>
2017-10-21 11:53:46 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var x = 100
+var y = 100
+
+function preload(){
+	
+}
+function setup(){
+	createCanvas(800,1000)
+	
+}
+function draw(){
+
+background(150);
+fill(225,0,0)	
+ellipse(x, y, 200, 200)
+x++;
+}
